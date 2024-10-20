7261c5db11e5a3e0ab72347951f31ca2857ab790
Magnus <magnus.h@wemail.no>
2024-10-20 16:43:04 +0200

    implemented get-functionality for books based on authors and isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,14 +18,23 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  if(isbn){
+    res.send(JSON.stringify(books[isbn], null, 4));
+  } else{
+    res.send("Book(s) does not exist!");
+  }
+
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let booksArr = Object.values(books);
+  let book = booksArr.filter((bk) => {return bk.author === author;});
+  if(book.length >= 1){
+    res.send(JSON.stringify(book, null, 4));
+  }
 });
 
 // Get all books based on title
